cc31a3ea71049a8fb3e18543926eb2d4c0dd2180
Scott Glover <glover.scott92@gmail.com>
2020-06-25 18:24:20 -0700

    setting up schema and seeds per homework readME

---
+++ seed.sql
@@ -0,0 +1,26 @@
+INSERT INTO department
+    (name)
+VALUES
+    ("Operations"),
+    ("Accounting"),
+    ("Loss Prevention"),
+    ("Human Resources");
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ("Operations Manager", 90000, 1),
+    ("Controller", 70000, 2),
+    ("Risk Manager", 50000, 3);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Scott", "Glover", 1, 1);
+
+SELECT *
+FROM department;
+SELECT *
+FROM role;
+SELECT *
+FROM employee;
